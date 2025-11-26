908824e985f19dcec6ec2bf959cd8170a14021d8
ncore-x <ncore.eng@gmail.com>
2025-11-26 19:26:26 +1100

    add crud operation

---
+++ api_v1/products/dependencies.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends, HTTPException, Path, status
+
+from api_v1.products import crud
+from core.models import db_helper, Product
+
+
+
+async def product_by_id(
+    product_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> Product:
+    product = await crud.get_product(session=session, product_id=product_id)
+    if product is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Product {product_id} not found!",
+        )
+    return product
--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,4 +1,13 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from asyncio import current_task
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
 from core.config import settings
 
 
@@ -14,6 +23,23 @@ class DatabaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        session = self.get_scoped_session()
+        yield session # type: ignore
+        await session.close()
 
 
 db_helper = DatabaseHelper(
